9c7a65ac52f83358dace8b5055ecfacba5536b4c
cschwirian <cschwirian@gmail.com>
2019-03-05 13:52:14 -0700

    Removed start object. Added some comments. Handled from_. Set to use upstream instead of downstream.

--- flaskui/generator.py
+++ flaskui/generator.py
@@ -115,22 +115,15 @@ default_args = {
 dag = DAG( "image_generation_dag", default_args=default_args, schedule_interval="@once" )
 
 prefix = 'source activate PDS-Pipelines && python /opt/conda/envs/PDS-Pipelines/scripts/isis3VarInit.py && source activate PDS-Pipelines && '
-
-start = BashOperator(
-    task_id="start",
-    bash_command="echo Starting...",
-    retries=3,
-    dag=dag
-)'''
+'''
 
     for dag_object in dag_objects:
         dag_string += "\n\n" + str( dag_object )
 
     dag_string += "\n"
-    dag_string += "\nstart.set_downstream(" + dag_objects[0].get_name() + ")"
 
-    for index in range( len( dag_objects ) - 1 ):
-        dag_string += "\n" + dag_objects[index].get_name() + ".set_downstream(" + dag_objects[index + 1].get_name() + ")"
+    for index in range( 1, len( dag_objects ) ):
+        dag_string += "\n" + dag_objects[index].get_name() + ".set_upstream(" + dag_objects[index - 1].get_name() + ")"
 
     return dag_string
 
@@ -228,51 +221,56 @@ def get_commands_from_file( recipe_file ):
 # Gets a list of DAG objects from a json file containing UI output, using json object
 def get_commands_from_json( recipe ):
 
-    # with open( recipe_filename, "r", ) as file:
-    #     recipe = json.load( file )
+    mission = recipe["mission"]
+    output = recipe["output"]
+    tasks = recipe["tasks"]
+    images = recipe["images"]
+    sources = recipe["sources"]
 
-        mission = recipe["mission"]
-        output = recipe["output"]
-        tasks = recipe["tasks"]
-        images = recipe["images"]
-        sources = recipe["sources"]
+    commands = []
+    dag_objects = []
 
-        commands = []
-        dag_objects = []
+    if( images == [] ):
+        for source in sources:
+            images.append( source.split("/")[-1] )
+            commands.append( WGETCommandObject( source.split("/")[-1].split(".")[0], source ) )
+            if(mission == "galileo_ssi_edr"):
+                commands.append( WGETCommandObject( source.split("/")[-1].split(".")[0] + "lbl", source.replace( ".img", ".lbl" ) ) )
 
-        if( images == [] ):
-            for source in sources:
-                images.append( source.split("/")[-1] )
-                commands.append( WGETCommandObject( source.split("/")[-1].split(".")[0], source ) )
-                if(mission == "galileo_ssi_edr"):
-                    commands.append( WGETCommandObject( source.split("/")[-1].split(".")[0] + "lbl", source.replace( ".img", ".lbl" ) ) )
-
-        for image in images:
-            file_index = 0
-            for task in tasks:
-                parameters = task[1]
-                for index in range( len( parameters ) ):
-                    if( parameters[index][0] == "from" ):
-                        if( "2isis" in task[0] ):
-                            if(task[0] == "gllssi2isis"):
-                                parameters[index][1] = "img/" + image.split(".")[0] + ".lbl"
-                            else:
-                                parameters[index][1] = "img/" + image
+    # Iterates over the supplied images.
+    for image in images:
+        file_index = 0
+        # Iterates over the supplied ISIS commands for the specified image.
+        for task in tasks:
+            parameters = task[1]
+            # Iterates over the parameters of the specified ISIS command.
+            for index in range( len( parameters ) ):
+                if( "from" in parameters[index][0] ):
+                    # Deals with the fact that some recipes have from_
+                    # as a parameter, despite it not being valid.
+                    if( parameters[index][0] == "from_" ):
+                        parameters[index][0] = parameters[index][0].replace( "_", "" )
+                    if( "2isis" in task[0] ):
+                        # Handles the need of .lbl files for gllssi.
+                        if(task[0] == "gllssi2isis"):
+                            parameters[index][1] = "img/" + image.split(".")[0] + ".lbl"
                         else:
-                            parameters[index][1] = "out/" + image.split(".")[0] + str(file_index) + ".cub"
-                    elif( parameters[index][0] == "to" ):
-                        file_index += 1
-                        if( task[0] == "isis2std" ):
-                            parameters[index][1] = "out/" + image.split(".")[0] + "." + parameters[-1][1]
-                        else:
-                            parameters[index][1] = "out/" + image.split(".")[0] + str(file_index) + ".cub"
+                            parameters[index][1] = "img/" + image
+                    else:
+                        parameters[index][1] = "out/" + image.split(".")[0] + str(file_index) + ".cub"
+                elif( parameters[index][0] == "to" ):
+                    file_index += 1
+                    if( task[0] == "isis2std" ):
+                        parameters[index][1] = "out/" + image.split(".")[0] + "." + parameters[-1][1]
+                    else:
+                        parameters[index][1] = "out/" + image.split(".")[0] + str(file_index) + ".cub"
 
-                commands.append( CommandObject( task[0] + image.split(".")[0], task[0], copy.deepcopy( parameters ) ) )
+            commands.append( CommandObject( task[0] + image.split(".")[0], task[0], copy.deepcopy( parameters ) ) )
 
-        for command in commands:
-            dag_objects.append( DAGObject( command ) )
+    for command in commands:
+        dag_objects.append( DAGObject( command ) )
 
-        return dag_objects
+    return dag_objects
 
 
 # Generates a pipeline job
@@ -285,7 +283,9 @@ def generate( data ):
     dag_string = generate_dag( dag_objects )
     timestamp = datetime.now().strftime( "%Y_%m_%d_%H_%M_%S" )
     with open( DAG_DIRECTORY + timestamp + ".py", "w" ) as job_file:
-        job_file.write( dag_string )
+       job_file.write( dag_string )
+    if( TEST ):
+        print( dag_string )
 
 
 # Tests generator library import
